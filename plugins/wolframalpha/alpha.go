e50753cc261642d2115e099575a8fb05d9fc4090
Gustavo Niemeyer <gustavo@niemeyer.net>
2014-07-19 15:17:22 -0300

    wolframalpha: polished parsing and displaying.

--- plugins/wolframalpha/alpha.go
+++ plugins/wolframalpha/alpha.go
@@ -2,17 +2,17 @@ package mup
 
 import (
 	"bytes"
+	"encoding/xml"
+	"io/ioutil"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
 	"time"
 
-	"gopkg.in/mgo.v2/bson"
 	"gopkg.in/mup.v0"
 	"gopkg.in/mup.v0/schema"
 	"gopkg.in/tomb.v2"
-	"gopkg.in/xmlpath.v2"
-	"io/ioutil"
-	"net/url"
-	"strings"
 )
 
 var Plugin = mup.PluginSpec{
@@ -26,6 +26,9 @@ var Commands = schema.Commands{{
 	Name: "infer",
 	Help: "Queries the WolframAlpha engine.",
 	Args: schema.Args{{
+		Name: "-all",
+		Type: schema.Bool,
+	}, {
 		Name: "query",
 		Flag: schema.Required | schema.Trailing,
 	}},
@@ -44,38 +47,31 @@ type alphaPlugin struct {
 	config   struct {
 		AppID    string
 		Endpoint string
-
-		HandleTimeout bson.DurationString
 	}
 }
-
-const defaultHandleTimeout = 500 * time.Millisecond
 
 func start(plugger *mup.Plugger) (mup.Stopper, error) {
 	p := &alphaPlugin{
 		plugger:  plugger,
-		commands: make(chan *mup.Command),
+		commands: make(chan *mup.Command, 5),
 	}
 	plugger.Config(&p.config)
 	if p.config.Endpoint == "" {
 		p.config.Endpoint = defaultEndpoint
-	}
-	if p.config.HandleTimeout.Duration == 0 {
-		p.config.HandleTimeout.Duration = defaultHandleTimeout
 	}
 	p.tomb.Go(p.loop)
 	return p, nil
 }
 
 func (p *alphaPlugin) Stop() error {
-	p.tomb.Kill(nil)
+	close(p.commands)
 	return p.tomb.Wait()
 }
 
 func (p *alphaPlugin) HandleCommand(cmd *mup.Command) error {
 	select {
 	case p.commands <- cmd:
-	case <-time.After(p.config.HandleTimeout.Duration):
+	default:
 		p.plugger.Sendf(cmd, "The WolframAlpha servers seem a bit sluggish right now. Please try again soon.")
 	}
 	return nil
@@ -83,31 +79,49 @@ func (p *alphaPlugin) HandleCommand(cmd *mup.Command) error {
 
 func (p *alphaPlugin) loop() error {
 	for {
-		select {
-		case cmd := <-p.commands:
-			p.handle(cmd)
-		case <-p.tomb.Dying():
-			return nil
+		cmd, ok := <-p.commands
+		if !ok {
+			break
 		}
+		p.handle(cmd)
 	}
 	return nil
 }
 
-var (
-	httpClient = http.Client{Timeout: time.Duration(5 * time.Second)}
-	textPath   = xmlpath.MustCompile("/queryresult[@success='true']/pod[@primary='true']/subpod/plaintext")
-	failPath   = xmlpath.MustCompile("/queryresult[@success='false']")
-	errorPath  = xmlpath.MustCompile("/queryresult[@error='true']/error/msg")
-)
+var httpClient = http.Client{Timeout: time.Duration(10 * time.Second)}
+
+type xmlResult struct {
+	Success bool     `xml:"success,attr"`
+	Error   string   `xml:"error>msg"`
+	Pods    []xmlPod `xml:"pod"`
+}
+
+type xmlPod struct {
+	Id      string      `xml:"id,attr"`
+	Title   string      `xml:"title,attr"`
+	Primary bool        `xml:"primary,attr"`
+	SubPods []xmlSubPod `xml:"subpod"`
+}
+
+type xmlSubPod struct {
+	Title string `xml:"title,attr"`
+	Text  string `xml:"plaintext"`
+}
 
 func (p *alphaPlugin) handle(cmd *mup.Command) {
-	var args struct{ Query string }
+	var args struct {
+		Query string
+		All   bool
+	}
 	cmd.Args(&args)
 
 	form := url.Values{
 		"appid":  {p.config.AppID},
 		"format": {"plaintext"},
 		"input":  {args.Query},
+	}
+	if cmd.Host != "" {
+		form["ip"] = []string{cmd.Host}
 	}
 
 	req, err := http.NewRequest("GET", p.config.Endpoint, nil)
@@ -117,12 +131,14 @@ func (p *alphaPlugin) handle(cmd *mup.Command) {
 	req.URL.RawQuery = form.Encode()
 
 	resp, err := httpClient.Do(req)
-	if err != nil {
-		p.plugger.Logf("Cannot query WolframAlpha: %v", err)
-		p.plugger.Sendf(cmd, "Cannot query WolframAlpha: %v", err)
+	if err == nil {
+		defer resp.Body.Close()
+	}
+	if err != nil || resp.StatusCode != 200 {
+		p.plugger.Logf("Error on request to WolframAlpha: %v", err)
+		p.plugger.Sendf(cmd, "WolframAlpha request failed. Please try again soon.")
 		return
 	}
-	defer resp.Body.Close()
 
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
@@ -131,36 +147,93 @@ func (p *alphaPlugin) handle(cmd *mup.Command) {
 		return
 	}
 
-	result, err := xmlpath.Parse(bytes.NewBuffer(data))
+	var result xmlResult
+	err = xml.Unmarshal(data, &result)
 	if err != nil {
 		p.plugger.Logf("Cannot parse WolframAlpha response: %v\nResponse:\n%s", err, data)
 		p.plugger.Sendf(cmd, "Cannot parse WolframAlpha response.")
 		return
 	}
 
-	if text, ok := textPath.String(result); ok {
-		p.plugger.Debugf("WolframAlpha result:\n%s", data)
-		p.plugger.Sendf(cmd, "%s", strip(text))
+	if result.Error != "" {
+		error := strip(result.Error)
+		p.plugger.Logf("WolframAlpha reported an error: %s", error)
+		p.plugger.Sendf(cmd, "WolframAlpha reported an error: %s", error)
 		return
 	}
 
-	if msg, ok := errorPath.String(result); ok {
-		msg = strip(msg)
-		p.plugger.Logf("WolframAlpha reported an error: %s", msg)
-		p.plugger.Sendf(cmd, "WolframAlpha reported an error: %s", msg)
-		return
-	}
+	p.plugger.Debugf("WolframAlpha result:\n%s", data)
 
-	if failPath.Exists(result) {
-		p.plugger.Debugf("WolframAlpha result:\n%s", data)
-		p.plugger.Sendf(cmd, "Cannot infer much out of this.")
-		return
+	var replied bool
+	var buf bytes.Buffer
+	if result.Success {
+		buf.Grow(256)
 	}
+	for _, pod := range result.Pods {
+		if pod.Id == "Input" {
+			continue
+		}
+		if !args.All && buf.Len() > 0 && !pod.Primary {
+			break
+		}
+		mark := buf.Len()
+		first := true
+		for _, subpod := range pod.SubPods {
+			text := strip(subpod.Text)
+			if text == "" {
+				continue
+			}
+			if first {
+				first = false
+				if buf.Len() > 0 {
+					buf.WriteString(" — ")
+				}
+				if pod.Title != "" && pod.Title != "Result" {
+					buf.WriteString(pod.Title)
+					buf.WriteString(": ")
+				}
+			} else {
+				if buf.Len() > 0 {
+					buf.WriteString("; ")
+				}
+			}
+			if subpod.Title != "" {
+				buf.WriteString(subpod.Title)
+				buf.WriteString(": ")
+			}
+			buf.WriteString(text)
+		}
+		if buf.Len() > 300 {
+			if buf.Len() - mark > 300 {
+				// The pod is too big by itself. Skip it.
+				buf.Truncate(mark)
+			} else {
+				p.send(cmd, string(buf.Next(mark)))
+				replied = true
+			}
+		}
+	}
+	if buf.Len() > 0 {
+		p.send(cmd, buf.String())
+		replied = true
+	}
+	if !replied {
+		if result.Success {
+			p.plugger.Logf("Unrecognized WolframAlpha result:\n%s", data)
+		}
+		p.plugger.Sendf(cmd, "Cannot infer much out of this. :-(")
+	}
+}
 
-	p.plugger.Logf("Cannot parse WolframAlpha response XML:\n%s", data)
-	p.plugger.Sendf(cmd, "Cannot parse WolframAlpha response.")
+var bars = regexp.MustCompile(`(?: \|){2,}`)
+
+func (p *alphaPlugin) send(cmd *mup.Command, text string) {
+	if strings.Contains(text, " | |") {
+		text = bars.ReplaceAllString(text, " |")
+	}
+	p.plugger.Sendf(cmd, "%s.", text)
 }
 
 func strip(text string) string {
-	return strings.Join(strings.Fields(text), " ")
+	return strings.Trim(strings.Join(strings.Fields(text), " "), ".")
 }
--- plugins/wolframalpha/alpha_test.go
+++ plugins/wolframalpha/alpha_test.go
@@ -12,6 +12,7 @@ import (
 
 	"gopkg.in/mgo.v2/bson"
 	"net/url"
+	"strings"
 )
 
 func Test(t *testing.T) { TestingT(t) }
@@ -21,43 +22,150 @@ var _ = Suite(&S{})
 type S struct{}
 
 type inferTest struct {
+	summary string
 	target  string
 	send    string
 	recv    string
+	recvAll []string
 	result  string
+	status  int
 	config  bson.M
 	targets []bson.M
 	form    url.Values
 }
 
 var inferTests = []inferTest{{
+	// Basic result displaying.
 	send:   "infer the query",
-	recv:   "PRIVMSG nick :the result",
-	result: successResult,
+	recv:   "PRIVMSG nick :the result.",
+	result: "<queryresult success='true'><pod><subpod><plaintext> the \n result </plaintext></subpod></pod></queryresult>",
 	config: bson.M{
 		"appid": "theid",
 	},
 	form: url.Values{
+		"ip":     {"host"},
 		"input":  {"the query"},
 		"format": {"plaintext"},
 		"appid":  {"theid"},
 	},
 }, {
-	send:   "infer the query",
-	recv:   "PRIVMSG nick :WolframAlpha reported an error: the error",
-	result: errorResult,
+	// Ignore the input entry, and ignore the Result title.
+	send: "infer the query",
+	recv: "PRIVMSG nick :result.",
+	result: `
+		 <queryresult success='true'>
+	         <pod id="Input"><subpod><plaintext>input</plaintext></subpod></pod>
+	         <pod title='Result'><subpod><plaintext>result</plaintext></subpod></pod>
+		 </queryresult>
+	`,
 }, {
-	send:   "infer the query",
-	recv:   "PRIVMSG nick :Cannot infer much out of this.",
-	result: failResult,
+	// Display only the primaries by default.
+	send: "infer the query",
+	recv: "PRIVMSG nick :primary one — primary two.",
+	result: `
+		 <queryresult success='true'>
+	         <pod primary='true'><subpod><plaintext>primary one</plaintext></subpod></pod>
+	         <pod primary='true'><subpod><plaintext>primary two</plaintext></subpod></pod>
+	         <pod><subpod><plaintext>non-primary</plaintext></subpod></pod>
+		 </queryresult>
+	`,
 }, {
-	send:   "infer the query",
-	recv:   "PRIVMSG nick :Cannot parse WolframAlpha response.",
-	result: "bogus",
+	// Display only the first when there's no primary.
+	send: "infer the query",
+	recv: "PRIVMSG nick :first.",
+	result: `
+		 <queryresult success='true'>
+	         <pod><subpod><plaintext>first</plaintext></subpod></pod>
+	         <pod><subpod><plaintext>second</plaintext></subpod></pod>
+		 </queryresult>
+	`,
 }, {
+	// Display all when explicitly requested.
+	send: "infer -all the query",
+	recv: "PRIVMSG nick :primary — non-primary.",
+	result: `
+		 <queryresult success='true'>
+	         <pod primary='true'><subpod><plaintext>primary</plaintext></subpod></pod>
+	         <pod><subpod><plaintext>non-primary</plaintext></subpod></pod>
+		 </queryresult>
+	`,
+}, {
+	// Skip empty subpods, even if it's a primary.
+	send: "infer the query",
+	recv: "PRIVMSG nick :non-empty.",
+	result: `
+		 <queryresult success='true'>
+	         <pod primary='true'><subpod><plaintext></plaintext></subpod></pod>
+	         <pod><subpod><plaintext>non-empty</plaintext></subpod></pod>
+		 </queryresult>
+	`,
+}, {
+	// Display titles.
+	send: "infer the query",
+	recv: "PRIVMSG nick :Pod one: Sub one: one.one; Sub two: one.two — Pod two: Sub one: two.one; Sub two: two.two.",
+	result: `
+		 <queryresult success='true'>
+	         <pod primary='true' title='Pod one'>
+		   <subpod title='Sub one'><plaintext>one.one</plaintext></subpod>
+		   <subpod title='Sub two'><plaintext>one.two</plaintext></subpod>
+		 </pod>
+	         <pod primary='true' title='Pod two'>
+		   <subpod title='Sub one'><plaintext>two.one</plaintext></subpod>
+		   <subpod title='Sub two'><plaintext>two.two</plaintext></subpod>
+		 </pod>
+		 </queryresult>
+	`,
+}, {
+	// Break down long pods, and skip extremely long ones.
+	send: "infer the query",
+	recvAll: []string{
+		"PRIVMSG nick :before — " + lorem + " — middle.",
+		"PRIVMSG nick : — " + lorem + " — after.",
+	},
+	result: `
+		 <queryresult success='true'>
+	         <pod primary='true'><subpod><plaintext>before</plaintext></subpod></pod>
+	         <pod primary='true'><subpod><plaintext>` + lorem + `</plaintext></subpod></pod>
+	         <pod primary='true'><subpod><plaintext>` + lorem + lorem + `</plaintext></subpod></pod>
+	         <pod primary='true'><subpod><plaintext>middle</plaintext></subpod></pod>
+	         <pod primary='true'><subpod><plaintext>` + lorem + `</plaintext></subpod></pod>
+	         <pod primary='true'><subpod><plaintext>after</plaintext></subpod></pod>
+		 </queryresult>
+	`,
+}, {
+	// If it's just an extremely long pod, show nothing.
+	send: "infer the query",
+	recv: "PRIVMSG nick :Cannot infer much out of this. :-(",
+	result: `
+		 <queryresult success='true'>
+	         <pod primary='true'><subpod><plaintext>` + lorem + lorem + `</plaintext></subpod></pod>
+		 </queryresult>
+	`,
+}, {
+	// No relevant meaning understood from the input.
+	send:   "infer the query",
+	recv:   "PRIVMSG nick :Cannot infer much out of this. :-(",
+	result: "<queryresult success='false'></queryresult>",
+}, {
+	// Apparent success with no parseable result.
 	send:   "infer the query",
 	recv:   "PRIVMSG nick :Cannot parse WolframAlpha response.",
 	result: "<queryresult success='true'><queryresult>",
+}, {
+	// Non-200 status code from endpoint.
+	send:   "infer the query",
+	recv:   "PRIVMSG nick :WolframAlpha request failed. Please try again soon.",
+	status: 500,
+}, {
+	// Detailed error result from service.
+	send:   "infer the query",
+	recv:   "PRIVMSG nick :WolframAlpha reported an error: the error",
+	result: `<queryresult error='true'><error><msg>the error</msg></error></queryresult>`,
+}, {
+	// Non-XML response.
+	send:   "infer the query",
+	recv:   "PRIVMSG nick :Cannot parse WolframAlpha response.",
+	result: "bogus",
 }}
 
 func (s *S) SetUpTest(c *C) {
@@ -76,6 +184,7 @@ func (s *S) TestInfer(c *C) {
 
 		server := &alphaServer{
 			result: test.result,
+			status: test.status,
 		}
 		server.Start()
 		if test.config == nil {
@@ -90,7 +199,13 @@ func (s *S) TestInfer(c *C) {
 		tester.Sendf(test.target, "%s", test.send)
 
 		c.Check(tester.Stop(), IsNil)
-		c.Check(tester.Recv(), Equals, test.recv)
+
+		if test.recv != "" {
+			c.Check(tester.Recv(), Equals, test.recv)
+		}
+		if test.recvAll != nil {
+			c.Check(tester.RecvAll(), DeepEquals, test.recvAll)
+		}
 
 		server.Stop()
 
@@ -106,6 +221,7 @@ func (s *S) TestInfer(c *C) {
 
 type alphaServer struct {
 	result string
+	status int
 	form   url.Values
 
 	server *httptest.Server
@@ -128,38 +244,17 @@ func (s *alphaServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	if req.URL.Path != "/" {
 		panic("Got unexpected request for " + req.URL.Path + " in test alphaServer")
 	}
+	if s.status != 0 {
+		w.WriteHeader(s.status)
+	}
 	s.form = req.Form
 	w.Write([]byte(s.result))
 }
 
-var successResult = `
-<?xml version='1.0' encoding='UTF-8'?>
-<queryresult success='true'>
-  <pod><subpod><plaintext>Not this one.</plaintext></subpod></pod>
-  <pod primary="true"><subpod>
-  <plaintext>
-  the
-  result 
-  </plaintext>
-  </subpod></pod>
-  <pod><subpod><plaintext>Not this one.</plaintext></subpod></pod>
-</queryresult>
-`
+var lorem = strings.Replace(`
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+ex ea commodo consequat
+`, "\n", "", -1)
 
-var errorResult = `
-<?xml version='1.0' encoding='UTF-8'?>
-<queryresult error='true'>
- <error>
-  <code>1</code>
-  <msg>
-  the
-  error
-  </msg>
- </error>
-</queryresult>
-`
-
-var failResult = `
-<?xml version='1.0' encoding='UTF-8'?>
-<queryresult success='false'></queryresult>
-`
