5c0906d592bff12dc057de77a56acd00a52505f2
Gustavo Niemeyer <gustavo@niemeyer.net>
2014-08-10 15:28:02 -0300

    admin: half-baked login command

--- plugins/admin/admin.go
+++ plugins/admin/admin.go
@@ -1,8 +1,11 @@
 package admin
 
 import (
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"gopkg.in/mup.v0"
 	"gopkg.in/mup.v0/schema"
+	"time"
 )
 
 var Plugin = mup.PluginSpec{
@@ -13,6 +16,13 @@ var Plugin = mup.PluginSpec{
 }
 
 var Commands = schema.Commands{{
+	Name: "login",
+	Help: "Authenticates with the bot.",
+	Args: schema.Args{{
+		Name: "password",
+		Flag: schema.Required | schema.Trailing,
+	}},
+}, {
 	Name: "sendraw",
 	Help: `Sends the provided text as a raw IRC protocol message.
 	
@@ -20,10 +30,8 @@ var Commands = schema.Commands{{
 	`,
 	Args: schema.Args{{
 		Name: "-account",
-		Type: schema.String,
 	}, {
 		Name: "text",
-		Type: schema.String,
 		Flag: schema.Required | schema.Trailing,
 	}},
 }}
@@ -34,10 +42,18 @@ func init() {
 
 type adminPlugin struct {
 	plugger *mup.Plugger
+
+	loggedIn map[string]bool
+
+	loginAttemptStart time.Time
+	loginAttemptCount int
 }
 
 func start(plugger *mup.Plugger) mup.Stopper {
-	return &adminPlugin{plugger: plugger}
+	return &adminPlugin{
+		plugger:  plugger,
+		loggedIn: make(map[string]bool),
+	}
 }
 
 func (p *adminPlugin) Stop() error {
@@ -45,6 +61,93 @@ func (p *adminPlugin) Stop() error {
 }
 
 func (p *adminPlugin) HandleCommand(cmd *mup.Command) {
+	switch cmd.Name() {
+	case "login":
+		p.login(cmd)
+	case "sendraw":
+		p.sendraw(cmd)
+	default:
+		p.plugger.Sendf(cmd, "I have a bug. Command %q exists and I don't know how to handle it.", cmd.Name())
+	}
+}
+
+const (
+	burstQuota   = 3
+	burstWindow  = 15 * time.Second
+	burstPenalty = 30 * time.Second
+)
+
+type userInfo struct {
+	Account      string
+	Nick         string
+	Password     string
+	AttemptStart time.Time
+	AttemptCount int
+}
+
+func (p *adminPlugin) login(cmd *mup.Command) {
+	var args struct{ Password string }
+	cmd.Args(&args)
+
+	session, c := p.plugger.Collection("", 0)
+	defer session.Close()
+
+	users := session.DB(c.Database.Name).C("users")
+	query := bson.D{{"account", cmd.Account}, {"nick", cmd.Nick}}
+
+	var user userInfo
+	err := users.Find(query).One(&user)
+	if err == mgo.ErrNotFound {
+		p.plugger.Sendf(cmd, "Nope.")
+		return
+	}
+	if err != nil {
+		p.plugger.Logf("Cannot get user details for nick %q at %s: %v", cmd.Nick, cmd.Account, err)
+		p.plugger.Sendf(cmd, "Oops: there was an error while checking your details.")
+		return
+	}
+	window := burstWindow
+	if user.AttemptCount >= burstQuota {
+		window = burstPenalty
+	}
+	if user.AttemptStart.Before(time.Now().Add(-window)) {
+		p.plugger.Logf("resetting quota")
+		user.AttemptCount = 0
+		user.AttemptStart = time.Now()
+	}
+	user.AttemptCount++
+	if user.AttemptCount > burstQuota {
+		p.plugger.Sendf(cmd, "Slow down.")
+		return
+	}
+	// TODO Use scrypt for passwords instead.
+	if user.Password != args.Password {
+		err := users.Update(query, bson.D{{"$set", bson.D{{"attemptstart", user.AttemptStart}, {"attemptcount", user.AttemptCount}}}})
+		if err != nil {
+			p.plugger.Logf("Cannot update login attempt for nick %q at %s: %v", cmd.Nick, cmd.Account, err)
+			p.plugger.Sendf(cmd, "Oops: there was an error while recording your login attempt.")
+		} else {
+			p.plugger.Sendf(cmd, "Nope.")
+		}
+		return
+	}
+	p.plugger.Sendf(cmd, "Okay.")
+	p.loggedIn[cmd.Nick] = true
+}
+
+func (p *adminPlugin) checkLogin(cmd *mup.Command) bool {
+	if p.loggedIn[cmd.Nick] {
+		return true
+	}
+	p.plugger.Sendf(cmd, "Must login for that.")
+	return false
+}
+
+func (p *adminPlugin) sendraw(cmd *mup.Command) {
+	if !p.checkLogin(cmd) {
+		return
+	}
+
 	var args struct{ Account, Text string }
 	cmd.Args(&args)
 	if args.Account == "" {
--- plugins/admin/admin_test.go
+++ plugins/admin/admin_test.go
@@ -1,44 +1,209 @@
 package admin_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	. "gopkg.in/check.v1"
 	"gopkg.in/mup.v0"
-	_ "gopkg.in/mup.v0/plugins/admin"
+	"gopkg.in/mup.v0/plugins/admin"
 )
 
 func Test(t *testing.T) { TestingT(t) }
 
-var _ = Suite(&SendRawSuite{})
+var _ = Suite(&AdminSuite{})
 
-type SendRawSuite struct{}
+type AdminSuite struct {
+	dbserver mup.DBServerHelper
+}
+
+func (s *AdminSuite) SetUpSuite(c *C) {
+	s.dbserver.SetPath(c.MkDir())
+}
+
+func (s *AdminSuite) TearDownSuite(c *C) {
+	s.dbserver.Stop()
+}
+
+func (s *AdminSuite) SetUpTest(c *C) {
+	mup.SetLogger(c)
+	mup.SetDebug(true)
+}
+
+func (s *AdminSuite) TearDownTest(c *C) {
+	s.dbserver.Wipe()
+	mup.SetLogger(nil)
+	mup.SetDebug(false)
+}
 
 type adminTest struct {
-	send []string
-	recv []string
+	summary string
+	target  string
+	send    []string
+	recv    []string
+	users   []userInfo
+	login   bool
 }
 
 var adminTests = []adminTest{
 	{
-		[]string{"sendraw"},
-		[]string{"PRIVMSG nick :Oops: missing input for argument: text"},
+		summary: "Good login",
+		users:   []userInfo{testUser},
+		send:    []string{"login thesecret"},
+		recv:    []string{"PRIVMSG nick :Okay."},
 	}, {
-		[]string{"sendraw NOTICE foo :text"},
-		[]string{"NOTICE foo :text", "PRIVMSG nick :Done."},
+		summary: "Attempt login with missing user",
+		send:    []string{"login thesecret"},
+		recv:    []string{"PRIVMSG nick :Nope."},
 	}, {
-		[]string{"sendraw -account=other PRIVMSG bar :text"},
-		[]string{"[other] PRIVMSG bar :text", "PRIVMSG nick :Done."},
+		summary: "Attempt login with bad password",
+		users:   []userInfo{testUser},
+		send:    []string{"login badsecret"},
+		recv:    []string{"PRIVMSG nick :Nope."},
+	}, {
+		summary: "Burst control: quota limit",
+		users: []userInfo{{
+			Account:           "test",
+			Nick:              "nick",
+			Password:          "thesecret",
+			AttemptStartDelta: -admin.BurstWindow + 2*time.Second,
+			AttemptCount:      admin.BurstQuota - 1,
+		}},
+		send: []string{"login badsecret", "login thesecret"},
+		recv: []string{"PRIVMSG nick :Nope.", "PRIVMSG nick :Slow down."},
+	}, {
+		summary: "Burst control: successful logins do not affect quota",
+		users: []userInfo{{
+			Account:      "test",
+			Nick:         "nick",
+			Password:     "thesecret",
+			AttemptCount: admin.BurstQuota - 1,
+		}},
+		send: []string{"login thesecret", "login thesecret"},
+		recv: []string{"PRIVMSG nick :Okay.", "PRIVMSG nick :Okay."},
+	}, {
+		summary: "Burst control: normal window expired",
+		users: []userInfo{{
+			Account:           "test",
+			Nick:              "nick",
+			Password:          "thesecret",
+			AttemptStartDelta: -admin.BurstWindow - 1*time.Second,
+			AttemptCount:      admin.BurstQuota - 1,
+		}},
+		send: []string{"login badsecret", "login badsecret"},
+		recv: []string{"PRIVMSG nick :Nope.", "PRIVMSG nick :Nope."},
+	}, {
+		summary: "Burst control: penalty window expired",
+		users: []userInfo{{
+			Account:           "test",
+			Nick:              "nick",
+			Password:          "thesecret",
+			AttemptStartDelta: -admin.BurstPenalty - 1*time.Second,
+			AttemptCount:      admin.BurstQuota,
+		}},
+		send: []string{"login thesecret"},
+		recv: []string{"PRIVMSG nick :Okay."},
+	}, {
+		summary: "Burst control: penalty window",
+		users: []userInfo{{
+			Account:           "test",
+			Nick:              "nick",
+			Password:          "thesecret",
+			AttemptStartDelta: -admin.BurstPenalty + 2*time.Second,
+			AttemptCount:      admin.BurstQuota,
+		}},
+		send: []string{"login thesecret"},
+		recv: []string{"PRIVMSG nick :Slow down."},
+	}, {
+		summary: "Burst control: smoke test",
+		users:   []userInfo{testUser},
+		send: []string{
+			"login badsecret",
+			"login badsecret",
+			"login badsecret",
+			"login thesecret",
+			"login thesecret",
+		},
+		recv: []string{
+			"PRIVMSG nick :Nope.",
+			"PRIVMSG nick :Nope.",
+			"PRIVMSG nick :Nope.",
+			"PRIVMSG nick :Slow down.",
+			"PRIVMSG nick :Slow down.",
+		},
+	},
+
+	{
+		send: []string{"sendraw"},
+		recv: []string{"PRIVMSG nick :Oops: missing input for argument: text"},
+	}, {
+		send: []string{"sendraw PRIVMSG foo :text"},
+		recv: []string{"PRIVMSG nick :Must login for that."},
+	}, {
+		login: true,
+		send:  []string{"sendraw NOTICE foo :text"},
+		recv:  []string{"NOTICE foo :text", "PRIVMSG nick :Done."},
+	}, {
+		login: true,
+		send:  []string{"sendraw -account=other PRIVMSG bar :text"},
+		recv:  []string{"[other] PRIVMSG bar :text", "PRIVMSG nick :Done."},
 	},
 }
 
-func (s *SendRawSuite) TestSendRaw(c *C) {
+type userInfo struct {
+	Account           string
+	Nick              string
+	Password          string
+	AttemptStartDelta time.Duration
+	AttemptStart      time.Time
+	AttemptCount      int
+}
+
+var testUser = userInfo{Account: "test", Nick: "nick", Password: "thesecret"}
+
+func (s *AdminSuite) TestAdmin(c *C) {
 	for i, test := range adminTests {
-		c.Logf("Testing message #%d: %s", i, test.send)
-		tester := mup.NewPluginTester("admin")
-		tester.Start()
-		tester.SendAll("", test.send)
-		tester.Stop()
-		c.Assert(tester.RecvAll(), DeepEquals, test.recv)
+		summary := test.summary
+		if summary == "" {
+			summary = fmt.Sprint("%q", test.send)
+		}
+		c.Logf("Test #%d: %s", i, summary)
+		s.testAdmin(c, &test)
 	}
+}
+
+func (s *AdminSuite) testAdmin(c *C, test *adminTest) {
+	defer s.dbserver.Wipe()
+
+	session := s.dbserver.Session()
+	defer session.Close()
+
+	db := session.DB("")
+	users := db.C("users")
+
+	tester := mup.NewPluginTester("admin")
+	tester.SetDatabase(db)
+
+	now := time.Now()
+	for _, user := range test.users {
+		user.AttemptStart = now.Add(user.AttemptStartDelta)
+		err := users.Insert(user)
+		c.Assert(err, IsNil)
+	}
+	if test.login && len(test.users) == 0 {
+		err := users.Insert(testUser)
+		c.Assert(err, IsNil)
+	}
+
+	tester.Start()
+
+	if test.login {
+		tester.Sendf(test.target, "login thesecret")
+		c.Assert(tester.Recv(), Equals, "PRIVMSG nick :Okay.")
+	}
+
+	tester.SendAll("", test.send)
+	tester.Stop()
+	c.Assert(tester.RecvAll(), DeepEquals, test.recv)
 }
