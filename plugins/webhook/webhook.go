c9eb52ec2204f42a6e6a943c1cf5e92b4e17ed9f
Gustavo Niemeyer <gustavo@niemeyer.net>
2016-09-06 02:29:25 -0300

    Add webhook plugin.

---
+++ plugins/webhook/webhook.go
@@ -0,0 +1,177 @@
+package webhook
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+	"time"
+
+	"gopkg.in/mup.v0"
+	"gopkg.in/tomb.v2"
+)
+
+var Plugin = mup.PluginSpec{
+	Name: "webhook",
+	Help: `Starts an HTTP server to receive webhook-style payloads.
+	
+	The payload must be formatted according to Rocket Chat and Slack webhooks,
+	with a "payload" HTTP parameter that contains a JSON object with "text",
+	"channel_name", and "user_name" fields, and also a "token" field used for
+	authentication that must match one of the strings in the "tokens" list
+	in the plugin configuration. The message is only received by mup if the
+	message origin matches one of the plugin targets.
+
+	The address to listen on may be changed via the "addr" configuration
+	option. If not provided the address 0.0.0.0:10456 is used.
+	`,
+	Start: start,
+}
+
+func init() {
+	mup.RegisterPlugin(&Plugin)
+}
+
+type webhookPlugin struct {
+	mu       sync.Mutex
+	tomb     tomb.Tomb
+	plugger  *mup.Plugger
+	listener net.Listener
+	config   struct {
+		Token string
+		Nick  string
+		Addr  string
+	}
+}
+
+const defaultAddr = ":10456"
+
+func start(plugger *mup.Plugger) mup.Stopper {
+	p := &webhookPlugin{
+		plugger: plugger,
+	}
+	p.plugger.Config(&p.config)
+	if p.config.Nick == "" {
+		p.config.Nick = "mup"
+	}
+	if p.config.Addr == "" {
+		p.config.Addr = defaultAddr
+	}
+	p.tomb.Go(p.loop)
+	return p
+}
+
+func (p *webhookPlugin) Stop() error {
+	p.tomb.Kill(nil)
+	p.mu.Lock()
+	if p.listener != nil {
+		p.listener.Close()
+	}
+	p.mu.Unlock()
+	p.plugger.Logf("Waiting.")
+	return p.tomb.Wait()
+}
+
+func (p *webhookPlugin) loop() error {
+	first := true
+	for p.tomb.Alive() {
+		l, err := net.Listen("tcp", p.config.Addr)
+		if err != nil {
+			if first {
+				first = false
+				p.plugger.Logf("Cannot listen on %s (%v). Will keep retrying.", p.config.Addr, err)
+			}
+			time.Sleep(500 * time.Millisecond)
+			continue
+		}
+		p.plugger.Logf("Listening on %s.", p.config.Addr)
+
+		p.mu.Lock()
+		p.listener = l
+		p.mu.Unlock()
+
+		server := &http.Server{
+			Addr:         p.config.Addr,
+			ReadTimeout:  10 * time.Second,
+			WriteTimeout: 10 * time.Second,
+			Handler:      p,
+		}
+
+		err = server.Serve(l)
+		if p.tomb.Alive() {
+			p.tomb.Kill(err)
+		}
+		l.Close()
+	}
+	return nil
+}
+
+type payloadMessage struct {
+	Token       string `json:"token"`        // "jhkjK7Khwe7whekjhwe7lwlh"
+	Timestamp   string `json:"timestamp"`    // "2016-09-05T02:54:47.616Z"
+	Text        string `json:"text"`         // "Hello there"
+	ChannelID   string `json:"channel_id"`   // "3kjhkKQkjwekjwjwkJhkewjhqejKWM8Lvw"
+	ChannelName string `json:"channel_name"` // "#general" (not there for private)
+	UserID      string `json:"user_id"`      // "Kh41HKEqnjekqwekj"
+	UserName    string `json:"user_name"`    // "joe"
+	Bot         bool   `json:"bot"`
+}
+
+func (p *webhookPlugin) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	err := r.ParseForm()
+	if err != nil {
+		p.plugger.Logf("Got request with broken form.")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+		return
+	}
+
+	payloadData := r.Form.Get("payload")
+
+	if len(payloadData) == 0 || r.Method != "POST" {
+		p.plugger.Logf("Got request with empty payload (%d) or invalid method (%s)", len(payloadData), r.Method)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"success:": false, "message": "message must be POSTed as JSON inside the "payload" parameter."}`))
+		return
+	}
+
+	pmsg := payloadMessage{}
+	err = json.Unmarshal([]byte(payloadData), &pmsg)
+	if err != nil {
+		p.plugger.Logf("Cannot unmarshal provided JSON payload: %v", err)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"success:": false, "message": "cannot unmarshal provided JSON payload"}`))
+		return
+	}
+
+	if pmsg.Token != p.config.Token {
+		p.plugger.Logf("Invalid token received: %s", pmsg.Token)
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"success:": false, "message": "invalid token"}`))
+		return
+	}
+
+	if pmsg.UserName == "" || pmsg.Text == "" {
+		p.plugger.Logf("Invalid payload received: %s", string(payloadData))
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(`{"success:": false, "message": "must provide at least user_name and text"}`))
+		return
+	}
+	if pmsg.UserID == "" {
+		pmsg.UserID = "user"
+	}
+	if pmsg.ChannelName == "" {
+		pmsg.ChannelName = p.config.Nick
+	}
+
+	line := fmt.Sprintf(":%s!~%s@webhook PRIVMSG %s :%s", pmsg.UserName, pmsg.UserID, pmsg.ChannelName, pmsg.Text)
+	msg := mup.ParseIncoming("", p.config.Nick, "!", line)
+	p.plugger.Logf("Received message: %s", msg)
+	err = p.plugger.Handle(msg)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(`{"success:": false, "message": "cannot enqueue message"}`))
+		return
+	}
+}
---
+++ plugins/webhook/webhook_test.go
@@ -0,0 +1,102 @@
+package webhook_test
+
+import (
+	"testing"
+
+	"net/http"
+	"net/url"
+
+	"gopkg.in/mgo.v2/bson"
+	"gopkg.in/mup.v0"
+
+	. "gopkg.in/check.v1"
+
+	_ "gopkg.in/mup.v0/plugins/webhook"
+	"net"
+)
+
+func Test(t *testing.T) { TestingT(t) }
+
+var _ = Suite(&WebHookSuite{})
+
+type WebHookSuite struct{}
+
+func (s *WebHookSuite) SetUpSuite(c *C) {
+	mup.SetLogger(c)
+	mup.SetDebug(true)
+}
+
+func (s *WebHookSuite) TearDownSuite(c *C) {
+	mup.SetLogger(nil)
+	mup.SetDebug(false)
+}
+
+type webhookTest struct {
+	payload string
+	message string
+	config  bson.M
+	targets []bson.M
+}
+
+var webhookTests = []webhookTest{{
+	// Missing target.
+	payload: `{"token": "secret", "user_name": "nick", "text": "Hello"}`,
+	config:  bson.M{"token": "secret"},
+	message: ``,
+}, {
+	// Bad secret.
+	payload: `{"token": "bad", "user_name": "nick", "text": "Hello"}`,
+	config:  bson.M{"token": "secret"},
+	targets: []bson.M{{"account": "test"}},
+	message: ``,
+}, {
+	// All good.
+	payload: `{"token": "secret", "user_name": "nick", "text": "Hello"}`,
+	message: `:nick!~user@webhook PRIVMSG mup :Hello`,
+	config:  bson.M{"token": "secret"},
+	targets: []bson.M{{"account": "test"}},
+}, {
+	// In a channel.
+	payload: `{"token": "secret", "user_name": "nick", "channel_name": "#chan", "text": "Hello"}`,
+	message: `:nick!~user@webhook PRIVMSG #chan :Hello`,
+	config:  bson.M{"token": "secret"},
+	targets: []bson.M{{"account": "test"}},
+}, {
+	// Different account.
+	payload: `{"token": "secret", "user_name": "nick", "channel_name": "#chan", "text": "Hello"}`,
+	message: `[@other] :nick!~user@webhook PRIVMSG #chan :Hello`,
+	config:  bson.M{"token": "secret"},
+	targets: []bson.M{{"account": "other"}},
+}}
+
+func (s *WebHookSuite) TestIn(c *C) {
+	transport := &http.Transport{DisableKeepAlives: true}
+	client := http.Client{Transport: transport}
+
+	for i, test := range webhookTests {
+		c.Logf("Testing payload #%d: %s", i, test.payload)
+		tester := mup.NewPluginTester("webhook")
+		if test.config == nil {
+			test.config = bson.M{}
+		}
+		test.config["addr"] = ":10645"
+		tester.SetConfig(test.config)
+		tester.SetTargets(test.targets)
+		tester.Start()
+
+		for i := 0; i < 100; i++ {
+			conn, err := net.Dial("tcp", "localhost:10645")
+			if err == nil {
+				conn.Close()
+				break
+			}
+		}
+
+		resp, err := client.PostForm("http://localhost:10645/", url.Values{"payload": {test.payload}})
+		c.Assert(err, IsNil)
+		resp.Body.Close()
+
+		tester.Stop()
+		c.Assert(tester.RecvIncoming(), Equals, test.message)
+	}
+}
